6530680f4b33890dde4059ab7f887fcc133f271c
Mathew Ballard <mathew@mathewballard.com>
2015-07-24 11:46:27 -0400

    Modified scroll-fader to allow changing the opacity of ::after pseudo element. Moved image for Services section to a background image applied via ::after.

--- public/lib/scripts/scroll-fader.js
+++ public/lib/scripts/scroll-fader.js
@@ -1,8 +1,14 @@
-var containerFadingOut = $('.fader-fade-out'),
-	containerFadingIn = $('.fader-fade-in');
+var fadingOut = $('.content-overlay'),
+	fadingIn = $('.overlaying-content');
 
 $(window).on('scroll', function(){
-	var scrollPosition = $(this).scrollTop();
-	containerFadingOut.css({'opacity' : (1 - scrollPosition/containerFadingOut.height()/2)});
-	containerFadingIn.css({'opacity' : (0 + scrollPosition/containerFadingIn.height())});
+	var scrollPosition = $(this).scrollTop(),
+		fadingOutHeight = fadingOut.height();
+
+	if (1 - scrollPosition/fadingOutHeight <= 0)  {
+		$(fadingOut).addClass('transparent');
+	} else{
+		$(fadingOut).removeClass('transparent');
+	};
+	fadingIn.css({'opacity' : (0 + scrollPosition/fadingIn.height())});
 });
