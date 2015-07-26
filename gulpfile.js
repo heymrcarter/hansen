dd6d3b9b5a4a82b1d37f3b2b0691923089dceaa8
heymrcarter <heymrcarter14@gmail.com>
2015-07-26 17:02:57 -0400

    1.0 of gulpfile: default task builds js and scss compile-scss compile-js build

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,22 @@ var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	sourcemaps = require('gulp-sourcemaps'),
 	concat = require('gulp-concat'),
-	order = require('gulp-order'),
 	minify = require('gulp-minify-css'),
-	uglify = require('gulp-uglify'),
-	print = require('gulp-print'),
-	addsrc = require('gulp-add-src'),
-	jsBuild = require('./source/scripts/build.js');
+	uglify = require('gulp-uglify');
 	
-gulp.task('default', ['watch']);
+gulp.task('default', ['build']);
+
+gulp.task('build', ['compile-sass', 'compile-js']);
 
 gulp.task('compile-sass', function () {
-	return gulp.src(['bower_components/foundation/scss/*.scss' ,'source/scss/**/*.scss'])
-		.pipe(sourcemaps.init())
-		.pipe(sass())
-		.pipe(order([
+	return gulp
+		.src([
 			'bower_components/foundation/scss/normalize.scss',
 			'bower_components/foundation/scss/foundation.scss',
 			'source/scss/**/*.scss'
-		]))
+		])
+		.pipe(sourcemaps.init())
+		.pipe(sass())
 		.pipe(gutil.env.type === 'production' ? minify() : gutil.noop())
 		.pipe(concat('styles.css'))
 		.pipe(sourcemaps.write())
@@ -29,42 +27,34 @@ gulp.task('compile-sass', function () {
 });
 
 gulp.task('compile-js', function () {
-	var jsLibs = [
+	var order = [
 		'bower_components/jquery/dist/jquery.js',
 		'bower_components/pubsub-js/src/pubsub.js',
 		'bower_components/foundation/js/foundation/foundation.js',
 		'bower_components/foundation/js/foundation/foundation.magellan.js',
-		'bower_components/foundation/js/foundation/foundation.clearing.js',		
+		'bower_components/foundation/js/foundation/foundation.clearing.js',
+		'source/scripts/equalizer.js',
+		'source/scripts/scroll-fader.js',
+		'source/scripts/geocoder.js',
+		'source/scripts/map.js',
+		'source/scripts/regex.js',
+		'source/scripts/validator.js',
+		'source/scripts/inquiry.js',
+		'source/scripts/inquiry-validator.js',
+		'source/scripts/inquiry-map.js',
+		'source/scripts/alert.js',
+		'source/scripts/cover.js',
+		'source/scripts/error-list.js',
+		'source/scripts/form.js'		
 	];
 	
 	if (gutil.env.type !== 'production') {
-		jsLibs.push('bower_components/jquery-mockjax/src/jquery.mockjax.js');
+		order.push('bower_components/jquery-mockjax/src/jquery.mockjax.js');
 	}
 	
-	return gulp.src(jsLibs)
+	return gulp
+		.src(order)
 		.pipe(sourcemaps.init())
-		.pipe(addsrc.append('source/scripts/**/*.js'))
-		.pipe(order([
-			'bower_components/jquery/dist/jquery.js',
-			'bower_components/pubsub-js/src/pubsub.js',
-			'bower_components/foundation/js/foundation/foundation.js',
-			'bower_components/foundation/js/foundation/foundation.magellan.js',
-			'bower_components/foundation/js/foundation/foundation.clearing.js',
-			'source/scripts/equalizer.js',
-			'source/scripts/scroll-fader.js',
-			'source/scripts/geocoder.js',
-			'source/scripts/map.js',
-			'source/scripts/regex.js',
-			'source/scripts/validator.js',
-			'source/scripts/inquiry.js',
-			'source/scripts/inquiry-validator.js',
-			'source/scripts/inquiry-map.js',
-			'source/scripts/alert.js',
-			'source/scripts/cover.js',
-			'source/scripts/error-list.js',
-			'source/scripts/form.js'			
-		]))
-		.pipe(print())
 		.pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
 		.pipe(gutil.env.type === 'production' ? concat('hansen.min.js') : concat('hansen.js'))
 		.pipe(sourcemaps.write())
@@ -75,9 +65,4 @@ gulp.task('jshint', function () {
 	return gulp.src('source/scripts/**/*.js', ['jshint'])
 		.pipe(jshint())
 		.pipe(jshint.reporter('jshint-stylish'));
-});
-
-gulp.task('watch', function () {
-	gulp.watch('source/scripts/**/*.js', ['jshint']);
-	gulp.watch('source/scss/**/*/scss', ['compile-sass']);
 });
